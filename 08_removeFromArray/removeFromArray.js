f4761d216ea06cf18df8623469176f83707423ed
Vlad Dincă <dincavlad01@gmail.com>
2025-11-24 12:42:59 +0200

    Add solution 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rest) {
+    let j = 0
+    let caught = false; 
+    for (let i = 0; i<= array.length; i++){
+        if (array[i] === rest[j] && array[i] != undefined){
+            array.splice(i, 1);
+            caught=true
+            i--;
+        }
+        else if (caught === true && array.length === i  && rest[j]!= undefined){
+            // j++;
+            rest.splice(j, 1);
+            i=0;
+            caught = false;
+        } 
+        else if (i === array.length && caught === false && rest[j] != undefined){
+            // j++;
+            rest.splice(j, 1);
+            i=0;
+        }
+        
+    }
+    return array;
 };
 
 // Do not edit below this line
