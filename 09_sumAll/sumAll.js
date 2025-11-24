9210cb62a1a947d9f19bc9469f1a021774c4065d
Vlad Dincă <dincavlad01@gmail.com>
2025-11-24 13:12:20 +0200

    Add solution 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    
+    if (a < 0 || b < 0)
+        return 'ERROR'
+    else if  (Number.isInteger(a) == false || Number.isInteger(b) == false )
+        return 'ERROR'
+    else if (a > b){
+        for(let i = b; i<=a; i++){
+            sum+=i;
+        }
+        return sum;
+    }      
+    else if (b > a){
+        for(let i = a; i<=b; i++){
+            sum+=i;
+        }   
+        return sum;
+    }
 
 };
 
