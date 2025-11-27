d63a14adffc011ecdc4aff6a1bf2fd78355ac8bc
Vlad Dincă <dincavlad01@gmail.com>
2025-11-27 17:00:35 +0200

    Add solution 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const newArray = array.map((item) => (item.title));
+    return newArray;
 };
 
 // Do not edit below this line
