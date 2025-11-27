f23a2403fc5d6e0214ad6106f895b736084b4572
Vlad Dincă <dincavlad01@gmail.com>
2025-11-27 12:11:18 +0200

    Add solution 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b ;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumArray = 0;
+  for(let i in array)
+  {
+    sumArray += array[i];
+  }
+  return sumArray;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplyArray = 1;
+  for(let i in array)
+  {
+    multiplyArray *= array[i];
+  }
+  return multiplyArray;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = 1;
+  for(let i = 1; i <= exponent; i++){
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let result = 1;
+  if (num === 0)
+    return 1;
+  else if (num === 1)
+    return 1;
+  else{
+    for (let i = 1; i <= num; i++){
+      result *= i;
+    }
+    return result;
+  }
 	
 };
 
