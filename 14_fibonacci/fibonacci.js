50e0f348e7dd93e8e705a412485a638f62f99655
Vlad Dincă <dincavlad01@gmail.com>
2025-11-27 16:52:18 +0200

    14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    if (typeof(num) !== "number")
+        num = parseInt(num);
+    if (num < 0)
+        return "OOPS"
+    if (num === 0)
+        return 0;
+    
+    // let fibo;
+    first = 1;
+    prev = 0;
+    for(let i = 2 ; i<= num; i++){
+        let fibo = first + prev;
+        prev = first;
+        first = fibo;  
+    } 
+    return first;       
 
 };
 
